d2a1682df92f3f465e563f338a498032b8ede473
Zqwangmath <Grothendieck2173@outlook.com>
2024-01-02 15:30:52 +0800

    1.1

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -99,6 +99,6 @@ $(document).ready(function(){
 
 <script type="text/javascript">
 var message_Path = '/Live2dHistoire/live2d/';
-var home_Path = 'https://lovepde.life/'
-{/* var talkAPI = auxiliaryHost+"/talk.php"; */}
+
+var talkAPI = auxiliaryHost+"/talk.php";
 </script>
